96411dbcfe19a0a6581cf7ff528153bf59c0a667
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-05-22 18:00:17 +0200

    Bring TCG

---
+++ src/decorator_utils.py
@@ -0,0 +1,9 @@
+from pydash import flow
+
+
+def apply(*map_funcs):
+    def decorator(f):
+        def wrapper(*args, **kwarg):
+            return flow(*map_funcs)(f(*args, **kwarg))
+        return wrapper
+    return decorator
