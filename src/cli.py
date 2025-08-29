067ec7aea6bd5707337d71306867043df6c36ab3
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-08-29 13:30:08 +0200

    change help

--- src/cli.py
+++ src/cli.py
@@ -31,8 +31,8 @@ class CLI:
         # Main args
         parser.add_argument('args', nargs='*', help='Words to translate, language to translate from and languages to translate to')
         parser.add_argument('--words', '-words', '-w', nargs='+', default=[], help='Words to translate')
-        parser.add_argument('--from-lang', '--from', '-from', '-f', dest='from_lang', help=supported_langs_msg)
-        parser.add_argument('--to-lang', '--to', '-to', '-t', '-l', dest='to_langs', nargs='+', default=[], help=supported_langs_msg)
+        parser.add_argument('--from-lang', '--from', '-from', '-f', dest='from_lang', help=(lang_help := 'Languages supported by glosbe.com'))
+        parser.add_argument('--to-lang', '--to', '-to', '-t', '-l', dest='to_langs', nargs='+', default=[], help=lang_help)
         # Loop Control
         loop_control = parser.add_mutually_exclusive_group()
         loop_control.add_argument('--loop', action='store_true', default=None, help='Enter a translation loop')
