fb38c143f7cf2777396346c8ca1a0546c69f658c
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-09-09 15:58:31 +0200

    add the captcha chekc

--- src/scrapping/scrapping.py
+++ src/scrapping/scrapping.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from time import sleep
 from typing import Any, Optional, Callable
 
 from requests import Session, Response
 from requests.exceptions import HTTPError
 
 from .parsing import InflectionParser, TranslationParser, ParsingException, ParsedTranslation, ParsedDefinition, \
-    DefinitionParser
+    DefinitionParser, Parser, CaptchaException
 from .web_pathing import GlosbePather
 
 
@@ -30,12 +31,15 @@ class Scrapper:
         self.session: Optional[Session] = session
 
     def scrap(self, url: str, parse: Callable[[Response], list[ParsedTranslation | ParsingException] | ParsingException]) -> list[ParsedTranslation] | HTTPError | ParsingException:
+        #sleep(5)
         try:
             response = self.session.get(url, allow_redirects=True)
             response.raise_for_status()
         except HTTPError as e:
             return e
         else:
+            if Parser.is_captcha(response):
+                return CaptchaException('Captcha appeared, robot identified!')
             return parse(response)
 
     def scrap_main_translations(self, from_lang: str, to_lang: str, word: str) -> list[ParsedTranslation] | HTTPError | ParsingException:
