afb720a1c5cc3e1210d55957cd09755b55e6c031
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-09-17 15:18:55 +0200

    thoughts

--- src/cli.py
+++ src/cli.py
@@ -37,7 +37,11 @@ class Outstemming:
     def outstem(cls, complex_word: str) -> list:
         # TODO: anhi test (and improve for "normal[ize[d]]")
         # TODO: test: rett[ig[het]] to generate three words
-        flat_outstem = c().map(cls.outstem).flatten().uniq()
+        # TODO: [teil^nehmen]: [teil, nehmen, teilnehmen]  # TODO: test and how to handle both "|" and "^" together? Prohibit?
+        # TODO: [teil^nehmen|haben]  # I think: yeah
+        # TODO: Why not just [teil][nehmen]?
+        # Cause I want to take them literally out and not in the compound. 3 together would break, but maybe other operator would be better
+        flat_outstem = c().map(cls.outstem).flatten().filter().uniq()
         logging.debug(f'outstemming "{complex_word}"')
         if matched := cls.parenthesised(complex_word):
             logging.debug(f'matched "{matched}"')
