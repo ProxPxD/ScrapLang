87e32e4286654d900bd52868c8abb5b39fd0e608
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-11-16 21:54:43 +0100

    add cuda detection

--- src/lang_detecting/detecting.py
+++ src/lang_detecting/detecting.py
@@ -3,12 +3,24 @@ from src.lang_detecting.preprocessing.data import DataPreprocessor
 from src.lang_detecting.lang_predictor import LangPredictor
 from src.resouce_managing.file import FileMgr
 
+try:
+    import torch
+    has_torch = True
+except ImportError:
+    has_torch = False
+
+print(f'Torch is {("un", "")[has_torch]}available')
+print("CUDA available:", torch.cuda.is_available())
+if torch.cuda.is_available():
+    print("CUDA device count:", torch.cuda.device_count())
+    print("Current device:", torch.cuda.current_device())
+    print("Device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
+
+
 
 class Detector:
     def __init__(self):
         ...
-
-
 
 sp = DataPreprocessor()
 preprocessed = sp.process(FileMgr(Paths.VALID_DATA_FILE).load())
