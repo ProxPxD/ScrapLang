a8b445f83ff0ec9aaa32f1443f807155cc915401
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-11-27 16:15:07 +0100

    move initial selecting to advanced detector

--- src/lang_detecting/advanced_detecting/advanced_detector.py
+++ src/lang_detecting/advanced_detecting/advanced_detector.py
@@ -1,10 +1,18 @@
 from pandas import DataFrame
 
-import torch
-
 from src.lang_detecting.advanced_detecting.model import Moe
+from src.lang_detecting.preprocessing.data import C
 
 
 class AdvancedDetector:
     def __init__(self, lang_script: DataFrame):
-        self.moe = Moe(lang_script)
+        script_lang = lang_script.explode(C.SCRIPTS).rename(columns={C.SCRIPTS: C.SCRIPT})
+        script_lang = (
+            script_lang.groupby(C.SCRIPT)[C.LANG]
+            .apply(list)
+            .reset_index(name=C.LANGS)
+            .sort_values(C.SCRIPT)
+            .reset_index(drop=True)
+        )
+        ambiguous_script_langs = script_lang[script_lang[C.LANGS].map(len) > 1]
+        self.moe = Moe(ambiguous_script_langs)
--- src/lang_detecting/advanced_detecting/model.py
+++ src/lang_detecting/advanced_detecting/model.py
@@ -7,15 +7,18 @@ import torch.nn as nn
 from src.lang_detecting.preprocessing.data import C
 
 
+class Router(nn.Module):
+    ...
+
+class ScriptExpert(nn.Module):
+    ...
+
+class Combiner(nn.Module):
+    ...
+
+
 class Moe(nn.Module):
-    def __init__(self, lang_script: DataFrame):
+    def __init__(self, script_lang: DataFrame):
         super().__init__()
-        script_lang = lang_script.explode(C.SCRIPTS).rename(columns={C.SCRIPTS: C.SCRIPT})
-        script_lang = (
-            script_lang.groupby(C.SCRIPT)[C.LANG]
-            .apply(list)
-            .reset_index(name=C.LANGS)
-            .sort_values(C.SCRIPT)
-            .reset_index(drop=True)
-        )
+        # script_lang[]
         pass
