3d5c0958cb853f07952ac752a904dc1c54b1df8f
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-12-10 17:35:24 +0100

    Simplify main

--- src/translate.py
+++ src/translate.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
-import shlex
 import sys
-from dataclasses import dataclass
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
@@ -11,53 +9,19 @@ from src.constants import Paths
 from src.app_managing import AppMgr
 
 
-@dataclass(frozen=True)
-class ErrorMessages:
-    UNKNOWN_EXCEPTION: str = 'Unknown exception occurred!'
-    ATTRIBUTE_ERROR: str = 'Error! Please send logs to the creator'
-
-
 def main():
     try:
-        # Configurations.init()
         AppMgr(
             conf_path=Paths.CONF_FILE,
             valid_data_file=Paths.VALID_DATA_FILE,
             short_mem_file=Paths.SHORT_MEM_FILE,
             lang_script_file=Paths.LANG_SCRIPT_FILE
         ).run()
-        #Configurations.change_last_used_languages(*system.langs)
-        #Configurations.save_and_close()
+    except KeyboardInterrupt:
+        pass
     except:
         raise
-    # except AttributeError as err:
-    #     logging.error(traceback.format_exc())
-    #     TranslationPrinter.out(ErrorMessages.ATTRIBUTE_ERROR)
-    # except Exception as ex:
-    #     # TODO: in next((flag for flag in self._flags if flag.has_name(name))) of system parser add an exception to know that the flag has not been added. Similarly in sibling cli_elements
-    #     logging.exception(traceback.format_exc())
-    #     TranslationPrinter.out(ErrorMessages.UNKNOWN_EXCEPTION, end='\n')
-
-
-# TODO: test conj
-# TODO: test cconj
-def get_test_arguments():
-    return shlex.split('trans lalka pl en')
-    # return shlex.split('trans t dotyczyć pl en')  # exception
-    # return shlex.split('trans definicja pl -def')
-    # return shlex.split('t dać pl -c')
-    # return shlex.split('t sweter pl -c')
-    # return shlex.split('t machen de -c')
-    # TOOO: add test for "mieć pl de -c"
-    # TOOO: add test for "mieć pl -c"
-    # TODO: add test for no saved lang exception
-    # TODO: add test for mis_tok
-    # TODO: add legacy_tests for definitions
-    # TODO: t anomic en pl
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
+    main()
