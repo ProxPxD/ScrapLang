c809d3a66b684c03fa11e32d4ed5c40c5a2d748e
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-12-11 11:33:13 +0100

    make more descriptive

--- src/lang_detecting/advanced_detecting/advanced_detector.py
+++ src/lang_detecting/advanced_detecting/advanced_detector.py
@@ -1,9 +1,9 @@
 import logging
 from itertools import combinations
-from typing import Any
+from typing import Any, Collection
 
 from pandas import DataFrame
-from pydash import flow, spread
+from pydash import flow, spread, partial
 from torchgen.utils import OrderedSet
 
 from src.constants import Paths
@@ -27,12 +27,21 @@ class AdvancedDetector:
         self.moe = Moe(shary_script)
 
     @classmethod
+    def filter_any_shared_chars(cls, chars_group: Collection[str]):
+        # c().apply(lambda cs: combinations(map(set, cs), 2)).map(spread(op.and_)).reduce_(lambda a, b: a | b, set()).to_list().sort().join()
+        combineds = c(chars_group).apply(lambda cs: combinations(map(set, cs), 2))
+        any_shareds = combineds.map(spread(op.and_))
+        all_any_shareds = any_shareds.reduce(lambda a, b: a | b, set())
+        as_strs = all_any_shareds.to_list().sort().join()
+        return as_strs.value()
+
+    @classmethod
     def extract_shary_scripts(cls, lang_script: DataFrame) -> dict[str, dict[str, str | list]]:
         script_lang = lang_script.explode(LSC.SCRIPTS).rename(columns={LSC.SCRIPTS: LSC.SCRIPT})
-        sclc = script_common_langs_chars = script_lang.groupby(LSC.SCRIPT, as_index=False).agg({
+        sclc = script_common_langs_chars = (script_lang.groupby(LSC.SCRIPT, as_index=False).agg({
             LSC.LANG: flow(sorted, list),
-            LSC.CHARS: c().apply(lambda cs: combinations(map(set, cs), 2)).map(spread(op.and_)).reduce_(lambda a, b: a | b, set()).to_list().sort().join()
-        }).rename(columns={LSC.LANG: LSC.LANGS})
+            LSC.CHARS: cls.filter_any_shared_chars
+        }).rename(columns={LSC.LANG: LSC.LANGS}))
         sclc = sclc[(sclc[LSC.LANGS].map(len) > 1) & (sclc[LSC.CHARS].map(len) > 0)].reset_index(drop=True)
         shary_script = {row[LSC.SCRIPT]: {LSC.LANGS: row[LSC.LANGS], LSC.CHARS: ''.join(row[LSC.CHARS])} for idx, row in sclc.iterrows()}
         return shary_script
