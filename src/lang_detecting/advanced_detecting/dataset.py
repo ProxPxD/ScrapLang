ecbbc0cea366dbfd6c48bfdc32ff527e0a79a460
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-02-10 21:32:29 +0100

    fix filtering through record threshold

--- src/lang_detecting/advanced_detecting/dataset.py
+++ src/lang_detecting/advanced_detecting/dataset.py
@@ -1,16 +1,17 @@
+from __future__ import annotations
+
 import random
 import time
 from collections import OrderedDict
+from copy import copy
 from dataclasses import dataclass
 from functools import cached_property
 
 import pydash as _
 import torch
-import torch.nn.functional as F
 from GlotScript import sp
 from pandas import DataFrame
-from pydash import chain as c
-from pydash import flow
+from pydash import chain as c, flow
 from torch import Tensor
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset
@@ -71,10 +72,14 @@ class BucketChunkDataset(Dataset[list[int]]):
 
     def _process_data(self, data: DataFrame) -> DataFrame:
         data = self._prefilter(data)
-        data = self._form_grouped_model_form(data)
+        data = self._form_model_form(data)
         data = self._expand_rows(data)
         data = self._filter_bad_train_data(data)
-        data = data.sort_values([Cols.LEN, Cols.N_UNIQ])
+        data = data.groupby([Cols.KIND, Cols.TOKENS, Cols.SPECS], sort=False).agg({
+            VDC.WORD: flow(set, list, ''.join),  # Should be same as c().get(0), but this allows to detect discrepancies
+            VDC.LANG: flow(set, sorted, list),
+        }).reset_index()
+        #data = data.sort_values([Cols.LEN, Cols.N_UNIQ])
         pass
         # TODO: make it work with multikind langs like japanese
         #data = data[data.apply(lambda row: all(char in self._kinds_to_shared[row[Cols.KIND]] for char in row[VDC.WORD]), axis=1)]
@@ -82,22 +87,17 @@ class BucketChunkDataset(Dataset[list[int]]):
 
     def _prefilter(self, data: DataFrame) -> DataFrame:
         data = data[~data[VDC.IS_MAPPED]][[VDC.LANG, VDC.WORD]]
-        class_counts = data[VDC.LANG].value_counts()
-        numerous_enough = class_counts[class_counts >= self.conf.data.record_count_thresh].index
-        data = data[data[VDC.LANG].isin(numerous_enough)]
         return data
 
-    def _form_grouped_model_form(self, data: DataFrame) -> DataFrame:
+    def _form_model_form(self, data: DataFrame) -> DataFrame:
+        data = copy(data)
         data[Cols.KIND] = data[VDC.WORD].apply(lambda w: next(iter(sp(w)[-1]['details'].keys())))
         data[Cols.TOKENS] = data.apply(lambda row: self.tokenizer.tokenize_input(row[VDC.WORD], row[Cols.KIND]), axis=1)
         data[Cols.SPECS] = data.apply(lambda row: self.tokenizer.tokenize_spec_groups(row[VDC.WORD], row[Cols.KIND]), axis=1)
-        data = data.groupby([Cols.KIND, Cols.TOKENS, Cols.SPECS], sort=False).agg({
-            VDC.WORD: flow(set, list, ''.join),  # Should be same as c().get(0), but this allows to detect discrepancies
-            VDC.LANG: flow(set, sorted, list),
-        }).reset_index()
         return data
 
     def _expand_rows(self, data: DataFrame) -> DataFrame:
+        data = copy(data)
         data[Cols.DECODE] = data.apply(lambda row: self.tokenizer.detokenize_input(row[Cols.TOKENS], row[Cols.KIND]), axis=1)
         data[Cols.LEN] = data[Cols.TOKENS].str.len()
         data[Cols.N_UNIQ] = data[Cols.DECODE].apply(c().filter(self.tokenizer.unknown.__contains__).apply(len))
@@ -109,6 +109,9 @@ class BucketChunkDataset(Dataset[list[int]]):
         m_right_ratio = (data[Cols.LEN] - 3) / data[Cols.N_UNIQ] >= 2
         m_long = data[Cols.LEN] >= self.conf.data.input_len_thresh
         data = data[m_long & (~m_uniq | m_enough_non_uniq & m_right_ratio)]
+        class_counts = data[VDC.LANG].value_counts()
+        numerous_enough = class_counts[class_counts >= self.conf.data.record_count_thresh].index
+        data = data[data[VDC.LANG].isin(numerous_enough)]
         return data
 
     def _map_data_to_tensor_batches(self, data: DataFrame) -> list[TensorBatch]:
