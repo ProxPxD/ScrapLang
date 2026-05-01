b1076b7ad567a1f559575a5353ced7f32fe26957
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-05-01 20:20:34 +0200

    refac

--- src/scrapping/scrap_managing.py
+++ src/scrapping/scrap_managing.py
@@ -50,10 +50,9 @@ class ScrapMgr:
             if group.is_first_at_from_grammar():
                 yield self.scrap_grammar(from_lang, word)
             main = Outcome.empty()
-            to_word = None
             if group.is_translating():
                 main = self.scrap_main_translations(from_lang, to_lang, word)
-                to_word = main.results[0].word if main.is_success() else None
+            to_word = main.results[0].word if main.is_success() else None
             if group.is_first_at_to_inflection(main):  # TODO: test is_success (ex. lubieć -it instead of lubić)
                 yield self.scrap_inflections(to_lang, to_word)
             if group.is_first_at_to_grammar(main):
